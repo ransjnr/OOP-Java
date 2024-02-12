0fe8709a46b24c51aa784683b69160b454db5edb
Ransford Oppong <faradayfoundationgh@gmail.com>
2024-02-12 03:00:50 -0800

    exercise 1- creating shapes

---
+++ Lab3/exercise1/src/App.java
@@ -0,0 +1,15 @@
+public class App {
+    public static void main(String[] args){
+        System.out.println("**********");
+        System.out.println("*        *");
+        System.out.println("*        *");
+        System.out.println("*        *");
+        System.out.println("*        *");
+        System.out.println("*        *");
+        System.out.println("*        *");
+        System.out.println("*        *");
+        System.out.println("*        *");
+        System.out.println("**********");
+
+    }
+}
