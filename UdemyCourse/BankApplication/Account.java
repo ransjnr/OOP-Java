4dd0046b70d13c26f59b0993f60b85e4b082f399
Ransford Oppong <faradayfoundationgh@gmail.com>
2024-02-11 21:43:59 -0800

    bank application

--- UdemyCourse/BankApplication/Account.java
+++ UdemyCourse/BankApplication/Account.java
@@ -1,19 +1,43 @@
-public class Amount{
-    long acno;
-    String name;
-    float bal;
 
-    public account(){}
+class BankAccount{
+    private double balance;
 
-    public Account(long acno, String name, float bal){
-        this.acno = acno;
-        this.name = name;
-        this.float = bal;
+    public BankAccount(double openingBalance)
+    {
+        balance = openingBalance;
     }
 
-    //deposit method
-    void deposit(float amt){
-        this.bal = this.bal + amt;
-        System.out.println("Rs. "+amt+" Credited | Balance: "+this.bal);
+    public void deposit(double amount)
+    {
+        balance = balance + amount;
+    }
+
+    public void withdraw(double amount)
+    {
+        balance = balance - amount;
+    }
+
+    public void display()
+    {
+        System.out.println("balannce=" + balance);
+    }
+}
+
+class Account
+{
+
+    public static void main(String[] args)
+    {
+        BankAccount bal1 = new BankAccount(100.00);
+
+        System.out.print("Before transactions, ");
+        bal1.display();
+
+        bal1.deposit(74.35);
+        bal1.withdraw(20.00);
+
+
+        System.out.print("After transactions, ");
+        bal1.display();
     }
 }
